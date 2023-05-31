2713b894adead18dd25b1f106dc19c1d614e9d2d
quangtvhe153307 <quangtvhe153307@fpt.edu.vn>
2023-06-01 00:31:08 +0700

    update attack

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -8,21 +8,36 @@ public class Enemy : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [Header("Attributes")]
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private int health;
+
 
     private Transform target;
-    private int pathIndex; 
+    private int pathIndex;
+
+    //total number of change direction while travel along the path
+    private int changedDirectionTimes;
+
+    public int ChangedDirectionTimes
+    {
+        get { return changedDirectionTimes; }
+    }
     private void Start()
     {
         target = PathFinding.main.path[pathIndex];
         rb = GetComponent<Rigidbody2D>();
+
+        changedDirectionTimes = 0;
+
+        EventManager.AddListener(EventName.EnemyAttackedEvent, SubtractHealth);
     }
 
     private void Update()
     {
-        if(Vector2.Distance(target.position, transform.position) <= 0.1f)
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
         {
             pathIndex++;
-            if(pathIndex >= PathFinding.main.path.Length)
+            changedDirectionTimes++;
+            if (pathIndex >= PathFinding.main.path.Length)
             {
                 Destroy(gameObject);
                 return;
@@ -39,5 +54,15 @@ public class Enemy : MonoBehaviour
     {
         Vector2 direction = (target.position - transform.position).normalized;
         rb.velocity = direction * moveSpeed;
+    }
+
+    /// <summary>
+    /// Subtract enemy's healthy when bullet reached
+    /// </summary>
+    /// <param name="points">points to add</param>
+    public void SubtractHealth(int points)
+    {
+        //health -= points;
+        Debug.Log("Subtract 10 points");
     }
 }
--- Assets/Script/PathFinding.cs
+++ Assets/Script/PathFinding.cs
@@ -8,8 +8,56 @@ public class PathFinding : MonoBehaviour
 
     public Transform startPoint;
     public Transform[] path;
+
+    /// <summary>
+    /// Contains array of node that make up the path
+    /// </summary>
+    private Node[] nodes;
     private void Awake()
     {
         main = this;
+        InitialNodes();
+    }
+    /// <summary>
+    /// Return sqrDistanceToTheEnd of a gameObject
+    /// </summary>
+    /// <param name="gameObject">GameObject</param>
+    /// <returns>-1 if is not enemy or dont have enemy component</returns>
+    public float SqrDistanceToTheEnd(GameObject gameObject)
+    {
+        if (!gameObject.tag.Equals("Enemy")) return -1;
+
+        Enemy enemy = gameObject.GetComponent<Enemy>();
+        if (enemy is null) return -1;
+
+        int numberOfturned = enemy.ChangedDirectionTimes;
+
+        float sqrDistance = nodes[numberOfturned + 1].CostToTheEnd + Vector3.SqrMagnitude(nodes[numberOfturned + 1].Position - gameObject.transform.position);
+        return sqrDistance;
+    }
+    /// <summary>
+    /// Initial Node list for and cost To the next node
+    /// </summary>
+    private void InitialNodes()
+    {
+        if (path is null || path.Length == 0) return;
+        nodes = new Node[path.Length];
+
+        //Assign position for each node in nodes
+        for (int i = 0; i < path.Length; i++)
+        {
+            nodes[i] = new Node
+            {
+                Position = path[i].position
+            };
+        }
+
+        // Reverse traversal to calculate cost to the end of each node
+        // Cost the the end of current node = cost to the end of previous node + sqrMagnitude between 2 nodes
+        nodes[nodes.Length - 1].CostToTheEnd = 0;
+        for (int i = nodes.Length - 2; i >= 0; i--)
+        {
+            nodes[i].CostToTheEnd = nodes[i + 1].CostToTheEnd + Vector3.SqrMagnitude(nodes[i].Position - nodes[i + 1].Position);
+        }
     }
 }
